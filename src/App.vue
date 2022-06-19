78baaa8b891157d42993b165fc278c034f0be928
reskybayu234 <75589544+reskybayu234@users.noreply.github.com>
2022-06-19 11:15:11 +0700

    belajar vuex

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
+  </div>
+</template>
+
+<script>
+import ProductListOneVue from './components/ProductListOne.vue';
+import ProductListTwoVue from './components/ProductListTwo.vue';
+
+export default {
+  components : {
+    'product-list-one' : ProductListOneVue,
+    'product-list-two' : ProductListTwoVue
+  },
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="product-list-one">
+        <h2>Product List One</h2>
+        <ul>
+            <li v-for="(product,index) in saleProducts" v-bind:key="index">
+                <span class="name">{{ product.name }}</span>
+                <span class="price">${{ product.price }}</span>
+            </li>
+        </ul>
+        <button v-on:click="reducePrice(4)"> Reduce Price </button>
+    </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+import {mapGetters} from 'vuex';
+
+export default {
+        computed : {
+            products(){
+                return this.$store.state.products;
+            },
+            // saleProducts(){
+            //     return this.$store.getters.saleProducts;
+            // }
+            ...mapGetters([
+                'saleProducts'
+            ]),
+          
+        },
+        methods : {
+            // reducePrice (amount){
+            //     this.$store.dispatch('reducePrice',amount);
+            // }
+              ...mapActions([
+                'reducePrice'
+            ])
+        }
+    }
+</script>
+
+<style scoped>
+    #product-list-one {
+        background : #d1e4ff;
+        box-shadow: 1px 2px 3px rgba(0,0,0,0,2);
+        margin-bottom: 30px;
+        padding:10px 20px;
+    }
+
+    #product-list-one ul{
+        padding : 0;
+        list-style-type:none;
+    }
+
+    #product-list-one li{
+        margin-right: 10px;
+        margin-top:10px;
+        padding:20px;
+        background:rgba(255,255,255,0.7);
+    }
+
+    .price{
+        font-weight: bold;
+        color:#e8800c;
+        display:block
+    }
+</style>
